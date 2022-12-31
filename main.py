619eb0feecb92c073da8dee1e480e67d768deae0
S1im_Sh4dy <mihakurd@gmail.com>
2022-12-31 15:30:06 +0300

    download book covers.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import urllib3
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -10,6 +11,31 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
+
+
+def url_parse(url, params):
+    response = requests.get(url=urljoin(url, f'b{params["id"]}/'))
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    title = soup.find('div', {'id': 'content'}).find('h1').text.split('::')[0].strip()
+    image_url = urljoin(url, soup.find('div', {'id': 'content'}).find('div', {'class': 'bookimage'}).find('img').get('src'))
+    text = soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})[-1].text.strip()
+
+    return title, image_url, text
+
+
+def download_image(url, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+
+    filename = urlsplit(url).path.split('/')[-1]
+    response = requests.get(url=url)
+    response.raise_for_status()
+
+    with open(f'{folder}{filename}', 'wb') as file:
+        file.write(response.content)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -26,29 +52,16 @@ def download_txt(url, filename, folder='books/'):
     return os.path.join(folder, upd_filename)
 
 
-def url_parse(url, params):
-    response = requests.get(url=f'{url}/b{params["id"]}/')
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    title = soup.find('div', {'id': 'content'}).find('h1').text.split('::')[0].strip()
-    image = url + soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})[0].find('img').get('src')
-    text = soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})[-1].text.strip()
-
-    return title, image, text
-
-
 def main():
     url = 'https://tululu.org'
 
-    for id in range(1, 11):
+    for id in range(1, 10):
         params = {'id': id}
         try:
-            title, image, text = url_parse(url, params)
+            title, image_url, text = url_parse(url, params)
 
-            print(download_txt(url=f'{url}/txt.php?id={id}', filename=title))
+            # print(download_txt(url=f'{url}/txt.php?id={id}', filename=title))
+            download_image(image_url)
 
         except requests.exceptions.HTTPError:
             print(f'HTTP_error on id = {id}')
