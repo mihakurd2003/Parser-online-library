8198b6c6113453280bd1084d374593d1d0f90892
S1im_Sh4dy <mihakurd@gmail.com>
2022-12-31 18:56:30 +0300

    download comments and parse genres.

--- main.py
+++ main.py
@@ -23,19 +23,11 @@ def url_parse(url, params):
     title = soup.find('div', {'id': 'content'}).find('h1').text.split('::')[0].strip()
     image_url = urljoin(url, soup.find('div', {'id': 'content'}).find('div', {'class': 'bookimage'}).find('img').get('src'))
     text = soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})[-1].text.strip()
+    comment = list(map(lambda el: el.find('span', {'class': 'black'}).text.strip(), soup.find('div', {'id': 'content'}).find_all('div',
+                                                                                                                                 {'class': 'texts'})))
+    genre = list(map(lambda el: el.text.strip(), soup.find('div', {'id': 'content'}).find('span', {'class': 'd_book'}).find_all('a')))
 
-    return title, image_url, text
-
-
-def download_image(url, folder='images/'):
-    os.makedirs(folder, exist_ok=True)
-
-    filename = urlsplit(url).path.split('/')[-1]
-    response = requests.get(url=url)
-    response.raise_for_status()
-
-    with open(f'{folder}{filename}', 'wb') as file:
-        file.write(response.content)
+    return title, image_url, text, comment, genre
 
 
 def download_txt(url, filename, folder='books/'):
@@ -52,16 +44,38 @@ def download_txt(url, filename, folder='books/'):
     return os.path.join(folder, upd_filename)
 
 
+def download_image(url, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+
+    filename = urlsplit(url).path.split('/')[-1]
+    response = requests.get(url=url)
+    response.raise_for_status()
+
+    with open(f'{folder}{filename}', 'wb') as file:
+        file.write(response.content)
+
+
+def get_comments(url):
+    response = requests.get(url=url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    comment = list(map(lambda el: el.find('span', {'class': 'black'}).text.strip(), soup.find('div', {'id': 'content'}).find_all('div',
+                                                                                                                                 {'class': 'texts'})))
+    return comment
+
+
 def main():
     url = 'https://tululu.org'
 
     for id in range(1, 10):
         params = {'id': id}
         try:
-            title, image_url, text = url_parse(url, params)
+            title, image_url, text, comment, genre = url_parse(url, params)
 
-            # print(download_txt(url=f'{url}/txt.php?id={id}', filename=title))
-            download_image(image_url)
+            # download_txt(url=urljoin(url, f'/txt.php?id={id}'), filename=title)
+            # download_image(image_url)
+            print(comment, genre, sep='\n****************************\n')
 
         except requests.exceptions.HTTPError:
             print(f'HTTP_error on id = {id}')
