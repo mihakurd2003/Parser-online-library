af89cc1cb9f520cd9bed533531fb724a0ca8d89f
S1im_Sh4dy <mihakurd@gmail.com>
2023-01-25 00:36:17 +0300

    - Links added to the README.md - Added script description - Name conflict resolved - Скрипт сделан устойчивым к разрыву соединения - Added parser support for all relative addresses - Fixed plural in titles - Reduced use of parentheses - Added error protection in GET parameters - Bugs fixed in download_txt()

--- main.py
+++ main.py
@@ -14,37 +14,45 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def parse_book_page(html_content):
+def parse_book_page(html_content, url):
     soup = BeautifulSoup(html_content, 'lxml')
 
-    title, author = list(map(lambda el: el.strip(), soup.find('div', {'id': 'content'}).find('h1').text.split('::')))
-    image_url = urljoin('https://tululu.org', soup.find('div', {'id': 'content'}).find('div', {'class': 'bookimage'}).find('img').get('src'))
+    header_book = soup.find('div', {'id': 'content'}).find('h1').text
+    title, author = list(map(lambda el: el.strip(), header_book.split('::')))
+
+    image_url = urljoin(url, soup.find('div', {'id': 'content'}).find('div', {'class': 'bookimage'}).find('img').get('src'))
     text = soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})[-1].text.strip()
-    comment = list(map(lambda el: el.find('span', {'class': 'black'}).text.strip(), soup.find('div', {'id': 'content'}).find_all('div', {'class': 'texts'})))
-    genre = list(map(lambda el: el.text.strip(), soup.find('div', {'id': 'content'}).find('span', {'class': 'd_book'}).find_all('a')))
+
+    comment_block = soup.find('div', {'id': 'content'}).find_all('div', {'class': 'texts'})
+    comments = list(map(lambda el: el.find('span', {'class': 'black'}).text.strip(), comment_block))
+
+    genres_block = soup.find('div', {'id': 'content'}).find('span', {'class': 'd_book'}).find_all('a')
+    genres = list(map(lambda el: el.text.strip(), genres_block))
 
     return {
         'title': title,
         'author': author,
         'image_url': image_url,
         'text': text,
-        'comment': comment,
-        'genre': genre,
+        'comments': comments,
+        'genres': genres,
     }
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, filename, folder='books/', params=None):
     os.makedirs(folder, exist_ok=True)
 
-    upd_filename = sanitize_filename(filename)
-    response = requests.get(url=url)
+    upd_filename = sanitize_filename(filename) + '.txt'
+    response = requests.get(url=url, params=params)
     response.raise_for_status()
     check_for_redirect(response)
 
-    with open(f'{folder}{upd_filename}.txt', 'w', encoding='utf-8') as file:
+    path = os.path.join(folder, upd_filename)
+
+    with open(path, 'w', encoding='utf-8') as file:
         file.write(response.text)
 
-    return os.path.join(folder, upd_filename)
+    return path
 
 
 def download_image(url, folder='images/'):
@@ -61,25 +69,30 @@ def download_image(url, folder='images/'):
 def main():
     url = 'https://tululu.org'
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('--start_id', type=int, default=0)
-    arg_parser.add_argument('--end_id', type=int)
+    arg_parser.add_argument('--start_id', type=int, default=0, help='С этого id начинается скачивание книг')
+    arg_parser.add_argument('--end_id', type=int, help='До этого id скачиваются книги')
     args = arg_parser.parse_args()
     if not args.end_id:
         args.end_id = args.start_id + 10
 
-    for id in range(args.start_id, args.end_id):
+    for id_book in range(args.start_id, args.end_id):
+        url_book = urljoin(url, f'b{id_book}/')
+        payload = {'id': id_book}
         try:
-            response = requests.get(url=urljoin(url, f'b{id}/'))
+            response = requests.get(url=url_book)
             response.raise_for_status()
             check_for_redirect(response)
 
-            parsed_book = parse_book_page(response.text)
+            parsed_book = parse_book_page(response.text, url_book)
 
-            download_txt(url=urljoin(url, f'/txt.php?id={id}'), filename=parsed_book['title'])
+            download_txt(url=urljoin(url, '/txt.php'), params=payload, filename=parsed_book['title'])
             download_image(parsed_book['image_url'])
 
         except requests.exceptions.HTTPError:
-            print(f'HTTP_error or redirect on id = {id}')
+            print(f'HTTP_error or redirect on id = {id_book}')
+
+        except requests.exceptions.ConnectionError as connection_error:
+            print(str(connection_error))
 
 
 if __name__ == '__main__':
