fcdee2c01786ff1c0e699825fe8b992dac89aee3
S1im_Sh4dy <mihakurd@gmail.com>
2023-02-08 21:58:50 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
@@ -17,18 +18,21 @@ def main():
     with open('books.json', 'r', encoding='UTF-8') as file:
         books = json.loads(file.read())
 
+    os.makedirs('pages', exist_ok=True)
+
     for book in books:
         book['image_url'] = urlsplit(book['image_url']).path.split('/')[-1]
         book['url_book'] = re.sub(r'[^\w_ -]', '', book['title'])
 
-    books = list(chunked(books, 2))
+    books = list(chunked(list(chunked(books, 2)), 10))
 
-    rendered_page = template.render(
-        books=books,
-    )
+    for num_page, books_on_page in enumerate(books):
+        rendered_page = template.render(
+            books=books_on_page,
+        )
 
-    with open('index.html', 'w', encoding="UTF-8") as file:
-        file.write(rendered_page)
+        with open(f'pages/index{num_page+1}.html', 'w', encoding="UTF-8") as file:
+            file.write(rendered_page)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
